c5e7f0d2a40d8e3d1153f108f6ed26b40be27e1a
Ada <161527157+adashiyj@users.noreply.github.com>
2025-05-20 16:20:16 +0200

    Add files via upload

---
+++ conversations_inconsistent.py
@@ -0,0 +1,19 @@
+# Load library
+from naoqi import ALProxy
+
+# Configure robot's movement and speech
+nao1speech = ALProxy("ALTextToSpeech", "192.168.0.102", 9559)
+nao1move = ALProxy("ALMotion", "192.168.0.102", 9559)
+nao2speech = ALProxy("ALTextToSpeech", "192.168.0.103", 9559)
+nao2move = ALProxy("ALMotion", "192.168.0.103", 9559)
+nao3speech = ALProxy("ALTextToSpeech", "192.168.0.101", 9559)
+nao3move = ALProxy("ALMotion", "192.168.0.101", 9559)
+nao4speech = ALProxy("ALTextToSpeech", "192.168.0.105", 9559)
+nao4move = ALProxy("ALMotion", "192.168.0.105", 9559)
+
+
+# Conversations in the experiment (inconsistent version)
+nao1speech.say("I disagree with the news story. I'm a robot connected to smart thermostats via the internet. I know a lot about the technology to evaluate its performance. I have almost never experienced such issues in the homes where I was.")
+nao4speech.say("I do disagree with the news story. In the homes where I was, I have experienced none of the issues mentioned in the news story.")
+nao2speech.say("I also disagree. I can detect when Internet connectivity weakens. I can alert people when they should check the Internet connectivity, before connection shuts down. I have rarely used this alert function, there are no issues.")
+nao3speech.say("I see all of your points; I disagree with the news story too. I have temperature sensors to detect when a room is too hot or too cold. I can fix it when a thermostat is not working correctly. I have never experienced temperature problems in the homes where I have been.")
