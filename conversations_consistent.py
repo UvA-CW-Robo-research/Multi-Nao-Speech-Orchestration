8b6624dedf09dd594ff7e100f6289fe85434b642
Ada <161527157+adashiyj@users.noreply.github.com>
2025-06-11 11:30:58 +0200

    Add files via upload

--- conversations_consistent.py
+++ conversations_consistent.py
@@ -1,15 +1,70 @@
 # Load library
 from naoqi import ALProxy
 
-# Configure robot's movement and speech
+# Configure robot's movement, speech, tracker
 nao1speech = ALProxy("ALTextToSpeech", "192.168.0.102", 9559)
+nao1tracker = ALProxy("ALTracker", "192.168.0.102", 9559)
 nao1move = ALProxy("ALMotion", "192.168.0.102", 9559)
+
 nao2speech = ALProxy("ALTextToSpeech", "192.168.0.103", 9559)
+nao2tracker = ALProxy("ALTracker", "192.168.0.103", 9559)
 nao2move = ALProxy("ALMotion", "192.168.0.103", 9559)
+
 nao3speech = ALProxy("ALTextToSpeech", "192.168.0.101", 9559)
+nao3tracker = ALProxy("ALTracker", "192.168.0.101", 9559)
 nao3move = ALProxy("ALMotion", "192.168.0.101", 9559)
+
 nao4speech = ALProxy("ALTextToSpeech", "192.168.0.105", 9559)
+nao4tracker = ALProxy("ALTracker", "192.168.0.105", 9559)
 nao4move = ALProxy("ALMotion", "192.168.0.105", 9559)
+
+
+# Face tracker with head movement only
+nao1move.stiffnessInterpolation("HeadYaw", 1.0, 5.0) # time argument not so sure
+nao1tracker.registerTarget("Face", 5) # width parameter not so sure
+nao1tracker.track("Face")
+nao1tracker.setMode("Head")
+# print(nao1tracker.isActive())
+
+nao2move.stiffnessInterpolation("HeadYaw", 1.0, 5.0) # time argument not so sure
+nao2tracker.registerTarget("Face", 5) # width parameter not so sure
+nao2tracker.track("Face")
+nao2tracker.setMode("Head")
+# print(nao2tracker.isActive())
+
+nao3move.stiffnessInterpolation("HeadYaw", 1.0, 5.0) # time argument not so sure
+nao3tracker.registerTarget("Face", 5) # width parameter not so sure
+nao3tracker.track("Face")
+nao3tracker.setMode("Head")
+# print(nao3tracker.isActive())
+
+nao4move.stiffnessInterpolation("HeadYaw", 1.0, 5.0) # time argument not so sure
+nao4tracker.registerTarget("Face", 5) # width parameter not so sure
+nao4tracker.track("Face")
+nao4tracker.setMode("Head")
+# print(nao4tracker.isActive())
+
+
+# Modify speed
+nao1speech.setParameter("speed", 85)
+nao2speech.setParameter("speed", 85)
+nao3speech.setParameter("speed", 85)
+nao4speech.setParameter("speed", 85)
+
+
+# Modify voice
+nao1speech.setVoice("naoenu")
+nao2speech.setVoice("naoenu")
+nao3speech.setVoice("naoenu")
+nao4speech.setVoice("naoenu")
+
+# Modify pitch
+nao1speech.setParameter("pitchShift", 0.78)
+nao2speech.setParameter("pitchShift", 0.8)
+nao3speech.setParameter("pitchShift", 1)
+nao4speech.setParameter("pitchShift", 1)
+
+print(nao4speech.getAvailableVoices())
 
 
 # Conversations in the experiment (inconsistent version)
