d5e79415c7b4add2e72260d7c17a1c4cb013b37b
Ada <161527157+adashiyj@users.noreply.github.com>
2025-06-18 10:25:16 +0200

    Add files via upload
    Define the IPs and port number

---
+++ nao_connection_config.py
@@ -0,0 +1,9 @@
+# Define the IP addresses of each NAO robot
+nao1_ip = "192.168.0.102"  
+nao2_ip = "192.168.0.103"
+nao3_ip = "192.168.0.101"  
+nao4_ip = "192.168.0.105"  
+
+# Define the port number used to connect to NAOqi on each robot
+# NAOqi is the central software system that lets you control and communicate with NAO.
+port = 9559
