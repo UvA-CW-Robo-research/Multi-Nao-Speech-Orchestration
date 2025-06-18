d51b5dcde5f2da0479e0fcefbe0a45f314c10b0b
Ada <161527157+adashiyj@users.noreply.github.com>
2025-06-18 10:37:30 +0200

    Update nao_setup.py

--- nao_setup.py
+++ nao_setup.py
@@ -1,7 +1,7 @@
 # Load library
 from naoqi import ALProxy
 
-# Define the IP addresses of each NAO robot
+# Define the IP address of each NAO robot
 nao1_ip = "192.168.0.102"  
 nao2_ip = "192.168.0.103"
 nao3_ip = "192.168.0.101"  
@@ -15,19 +15,23 @@ port = 9559
 nao1speech = ALProxy("ALTextToSpeech", nao1_ip, port)
 nao1tracker = ALProxy("ALTracker", nao1_ip, port)
 nao1move = ALProxy("ALMotion", nao1_ip, port)
+nao1pose = ALProxy("ALRobotPosture", nao1_ip, port)
 
 # Initialize proxies for NAO2
 nao2speech = ALProxy("ALTextToSpeech", nao2_ip, port)
 nao2tracker = ALProxy("ALTracker", nao2_ip, port)
 nao2move = ALProxy("ALMotion", nao2_ip, port)
+nao2pose = ALProxy("ALRobotPosture", nao2_ip, port)
 
 # Initialize proxies for NAO3
 nao3speech = ALProxy("ALTextToSpeech", nao3_ip, port)
 nao3tracker = ALProxy("ALTracker", nao3_ip, port)
 nao3move = ALProxy("ALMotion", nao3_ip, port)
+nao3pose = ALProxy("ALRobotPosture", nao3_ip, port)
 
 # Initialize proxies for NAO4
 nao4speech = ALProxy("ALTextToSpeech", nao4_ip, port)
 nao4tracker = ALProxy("ALTracker", nao4_ip, port)
 nao4move = ALProxy("ALMotion", nao4_ip, port)
+nao4pose = ALProxy("ALRobotPosture", nao4_ip, port)
 
